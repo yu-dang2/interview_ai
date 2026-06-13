ad7cff19ac1436f276685ad89694c926fa8cc40e
hyeonJu-521 <GitHub ihyeonju04@gmail.com>
2026-06-13 21:07:51 +0900

    feat: resume_parser 노드 구현

---
+++ agent/graph/nodes/resume_parser.py
@@ -0,0 +1,15 @@
+"""
+resume_parser 노드
+작성: 이현주
+
+예진님 RESUME_PARSER_SYSTEM_PROMPT 사용하여 이력서 텍스트 → JSON 구조화.
+"""
+
+from agent.parsers.resume_parser_prompt import RESUME_PARSER_SYSTEM_PROMPT
+from graph.state import InterviewState
+from graph.utils import call_llm
+
+
+def resume_parser(state: InterviewState):
+    resume_parsed = call_llm(RESUME_PARSER_SYSTEM_PROMPT, state["resume_raw"])
+    return {"resume_parsed": resume_parsed}
